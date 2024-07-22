3737633ff7ccba773a1a81c463868cee2983f3ba
Aleksi Korsman <aleksi.korsman@aalto.fi>
2024-07-22 12:13:27 +0300

    add os.fsync argument

--- src/base.py
+++ src/base.py
@@ -409,7 +409,7 @@ def executeBuild(target, arch, prefix, build_dir, output_dir, nproc, pack_source
 		scriptfile.write(open(os.path.join(SCRIPTS_ROOT, "package-" + arch.split('-')[0] + ".sh"), 'r').read().encode())
 
 	scriptfile.flush()
-	os.fsync()
+	os.fsync(scriptfile)
 
 	log_step("Compiling ...")
 	params = ['docker', 
